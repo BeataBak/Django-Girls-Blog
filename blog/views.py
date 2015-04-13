61f9d3efe7d0a1fb5ebebf2842d971b82a7dedd9
Beata Bąk <beata@dirtymonkey.co.uk>
2015-04-13 15:46:26 +0100

    Add access restrictions to view for people not logged in

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
+from django.conf import settings
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    if not request.user.is_authenticated():
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        # return HttpResponseNotFound('<h1>No access for you!!!</h1>')
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>No access for you!!!</h1>')
+    else:
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
@@ -25,14 +34,18 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>No access for you!!!</h1>')
+
     else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('blog.views.post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
